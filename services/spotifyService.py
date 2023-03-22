73db3588d284c32a5cf63bfa57069b932b22ff87
Rajesh Sethi <rajesh.sethi@walmart.com>
2023-03-22 22:26:35 +0530

    parse response and return result

--- services/spotifyService.py
+++ services/spotifyService.py
@@ -3,4 +3,9 @@ from clients import spotifyClient
 from services import auth
 def getSongList(id,market):
         resp = spotifyClient.getTopSongsByArtist(id,market)
-        return resp.json()
+        topSongsDect = resp.json()
+        allTracks = topSongsDect['tracks']
+        trackNames = []
+        for track in allTracks:
+                trackNames.append(track['name'])
+        return trackNames
